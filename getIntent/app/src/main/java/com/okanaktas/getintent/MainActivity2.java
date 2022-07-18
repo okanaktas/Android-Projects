d9e832bd626ff74dd1d5a664bdc148e4fe876b38
Okan Aktaş <okanaktas02@gmail.com>
2022-07-18 07:18:51 +0300

    getIntent

---
+++ getIntent/app/src/main/java/com/okanaktas/getintent/MainActivity2.java
@@ -0,0 +1,33 @@
+package com.okanaktas.getintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textViewInput);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("userInput");
+        textView.setText(userName);
+
+
+    }
+
+    public void goToFirstPage (View view) {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+}
