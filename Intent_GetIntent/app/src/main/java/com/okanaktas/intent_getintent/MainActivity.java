7168c30c5eace8e1b9c19ef7c8f0545a7d0e18b4
Okan Aktaş <okanaktas02@gmail.com>
2022-07-31 11:01:30 +0300

    updatee

---
+++ Intent_GetIntent/app/src/main/java/com/okanaktas/intent_getintent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.okanaktas.intent_getintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
