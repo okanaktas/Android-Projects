d69e1c73340a4923fcfc3800c3f7cbec8e58f3d4
Okan Aktaş <okanaktas02@gmail.com>
2022-07-31 11:19:41 +0300

    getIntent

--- Intent_GetIntent/app/src/main/java/com/okanaktas/intent_getintent/MainActivity.java
+++ Intent_GetIntent/app/src/main/java/com/okanaktas/intent_getintent/MainActivity.java
@@ -2,13 +2,32 @@ package com.okanaktas.intent_getintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editTextTextPersonName);
+
+    }
+
+    public void goToActivity(View view){
+        name = editText.getText().toString(); //Bunu onCreate e yazarsan çalışmıyor.
+
+        Intent intent = new Intent(getApplicationContext(),MainActivity2.class);
+        intent.putExtra("name",name);
+        startActivity(intent);
     }
 }
---
+++ Intent_GetIntent/app/src/main/java/com/okanaktas/intent_getintent/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.okanaktas.intent_getintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+
+    String input;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+
+        input = intent.getStringExtra("name");
+
+        textView.setText(input);
+    }
+}
