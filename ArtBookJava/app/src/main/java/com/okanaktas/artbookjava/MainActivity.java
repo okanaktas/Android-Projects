8f13c32d3ecfe00e3908f8e1c544a748204f612d
Okan Aktaş <okanaktas02@gmail.com>
2022-08-06 20:04:14 +0300

    ArtBookJava

---
+++ ArtBookJava/app/src/main/java/com/okanaktas/artbookjava/MainActivity.java
@@ -0,0 +1,14 @@
+package com.okanaktas.artbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
