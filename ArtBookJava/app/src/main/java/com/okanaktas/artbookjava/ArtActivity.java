96416f8be42fc8ed472e1d3cdc741ff3d1f71201
Okan Aktaş <okanaktas02@gmail.com>
2022-08-06 20:48:48 +0300

    UI Create

---
+++ ArtBookJava/app/src/main/java/com/okanaktas/artbookjava/ArtActivity.java
@@ -0,0 +1,22 @@
+package com.okanaktas.artbookjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ArtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_art);
+    }
+
+    public void buttonSave(View view){
+
+    }
+    public void imageSelect(View view){
+
+    }
+}
--- ArtBookJava/app/src/main/java/com/okanaktas/artbookjava/MainActivity.java
+++ ArtBookJava/app/src/main/java/com/okanaktas/artbookjava/MainActivity.java
@@ -2,13 +2,22 @@ package com.okanaktas.artbookjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+
+import com.okanaktas.artbookjava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
     }
 }
